7354334436b37c6fbd5cfd0c966d26affaed7110
xingxingwang <1218761836@qq.com>
2016-11-13 12:57:20 +0800

    ruby install

--- rbenv.sh
+++ rbenv.sh
@@ -3,5 +3,10 @@
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc   && source .bashrc   
+source .bashrc 
+#ruby install 
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
 rbenv install 2.3.1
-echo "eval "$(rbenv init -)"" >> ~.bashrc 
+echo "eval "$(rbenv init -)"" >> ~/.bashrc 
+
+
